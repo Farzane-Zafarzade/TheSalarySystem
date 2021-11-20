8f5000444454fd16b430d8a10673f0cf69d37d2b
Sammy Wong <66samwon@gafe.molndal.se>
2021-11-20 17:04:08 +0100

    Correct Integration_User_Test

--- TheSalarySystemTests/IntegrationTest.cs
+++ TheSalarySystemTests/IntegrationTest.cs
@@ -12,7 +12,6 @@
         private UserTests useTest = new();
 
         [TestMethod()]
-
         public void Integration_Admin_TesT()
         {
             StartMenuTests.LoginTest("admin1", "admin1234", "admin1");
@@ -20,11 +19,13 @@
             adminTest.DeleteTest_DeleteAdminAccount_ReturnFalse("admin1", "admin1234", false);
         }
 
+        [TestMethod()]
         public void Integration_User_Test()
         {
             StartMenuTests.LoginTest("farzanezafar", "password1", "farzanezafar");
+            useTest.DeleteTest_DeleteOthersAccount_ShouldReturnFalse("davidberg", "david123", false);
             useTest.DeleteTest_DeleteOnesOwnAccount_ShouldReturnTrue("farzanezafar", "password1", true);
-            useTest.DeleteTest_DeleteOnesOwnAccount_ShouldReturnTrue("davidberg", "david123", false);
+            
         }
     }
 }
